504c41bdc4735832a3e921cca68df0235b36b61f
beinawair <notifier.id@gmail.com>
2022-03-12 18:36:28 +0800

    Updates

---
+++ precache-manifest.e148ae7a11df009a8e2bb0bf4220f47a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "44bcd3565f2de43b8611",
+    "url": "/bions/static/css/main.6aff8c92.chunk.css"
+  },
+  {
+    "revision": "44bcd3565f2de43b8611",
+    "url": "/bions/static/js/main.44bcd356.chunk.js"
+  },
+  {
+    "revision": "45ffd9ed5444b5efc626",
+    "url": "/bions/static/js/1.45ffd9ed.chunk.js"
+  },
+  {
+    "revision": "1dad266c5002c5d0aa3e",
+    "url": "/bions/static/js/runtime~main.1dad266c.js"
+  },
+  {
+    "revision": "69f35d382edb252e3d28631a7d611ec8",
+    "url": "/bions/index.html"
+  }
+];
