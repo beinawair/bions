8d695c2eac207ca1218d03be8bfc03a4d279e45e
beinawair <notifier.id@gmail.com>
2022-03-13 09:13:29 +0800

    fix linked file

--- src/components/About.js
+++ src/components/About.js
@@ -13,11 +13,11 @@ function About() {
                 </p>
             </div>
             <div className='about-images'>
-                <img className='img' src='/img/img01.png' alt=''/>
-                <img className='img' src='/img/img02.png' alt=''/>
-                <img className='img' src='/img/img03.png' alt=''/>
-                <img className='img' src='/img/img04.png' alt=''/>
-                <img className='img' src='/img/img05.png' alt=''/>
+                <img className='img' src={process.env.PUBLIC_URL + '/img/img01.png'} alt=''/>
+                <img className='img' src={process.env.PUBLIC_URL + '/img/img02.png'} alt=''/>
+                <img className='img' src={process.env.PUBLIC_URL + '/img/img03.png'} alt=''/>
+                <img className='img' src={process.env.PUBLIC_URL + '/img/img04.png'} alt=''/>
+                <img className='img' src={process.env.PUBLIC_URL + '/img/img05.png'} alt=''/>
             </div>
         </div>
     </div>
